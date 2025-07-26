1ae5e3e03019a3fdaa18e2aa9fe44273014537c2
woohoodai <woohoodai@gmail.com>
2025-07-27 00:46:51 +0800

    fix: 禁用postcss对 alignItems 的优化，它错误地更改了 .extra-actions 的样式

--- postcss.config.js
+++ postcss.config.js
@@ -7,7 +7,21 @@ module.exports = {
     postcssImport,
     postcssNesting,
     cssnano({
-      preset: "default",
+      preset: [
+        "default",
+        {
+          // 禁用 postcss-reduce-initial 插件
+          // reduceInitial: false,
+          // 或者更精确地控制哪些属性不被优化
+          reduceInitial: {
+            // 禁用 align-items 的优化
+            alignItems: false,
+            // 禁用其他可能影响布局的属性
+            // display: false,
+            // position: false,
+          }
+        },
+      ],
     }),
   ],
 };
